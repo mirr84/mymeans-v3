9c195e50def3c7ee9d7d3072680c72e1937621ab
mirr <mirr84@mail.ru>
2018-12-09 16:05:09 +0300

    123

--- server/api/news.js
+++ server/api/news.js
@@ -1,3 +1,11 @@
+const mysql = require('promise-mysql');
+
+const db_conf = {
+    host: 'y913929d.beget.tech',
+    user: 'y913929d_mymeans',
+    password: '123456',
+    database: 'y913929d_mymeans'
+}
 
 const get = ({id, res}) => {
 
@@ -11,111 +19,32 @@ const get = ({id, res}) => {
 
 const getList = ({offset, limit, filter, res}) => {
 
-    const t = (id, cardImg, cardTitle, cardSubtitle, cardText, button = false, body = false, inverse = false, color, style) =>
-        ({ id, cardImg, cardTitle, cardSubtitle, cardText, button, body, inverse, color, style })
+    // {"borderColor": "#333", "backgroundColor": "#333"}
 
-    const items = [
-        t(
-            1,
-            'https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180',
-            'Card title',
-            'Card subtitle',
-            'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            false,
-            false,
-            false,
-            null,
-            null
-        ),
-        t(
-            2,
-            'https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180',
-            null,
-            null,
-            null,
-            false,
-            false,
-            false,
-            null,
-            null
-        ),
-        t(
-            3,
-            null,
-            'Card title',
-            'Card subtitle',
-            'This card has supporting text below as a natural lead-in to additional content.',
-            true,
-            false,
-            false,
-            null,
-            null
-        ),
-        t(
-            4,
-            null,
-            null,
-            'Special Title Treatment',
-            'With supporting text below as a natural lead-in to additional content.',
-            true,
-            true,
-            true,
-            null,
-            {backgroundColor: '#333', borderColor: '#333'}
-        ),
-        t(
-            5,
-            'https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180',
-            'Card title',
-            'Card subtitle',
-            'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
-            true,
-            false,
-            false,
-            null,
-            null
-        ),
-        t(
-            6,
-            null,
-            'Special Title Treatment',
-            'Card subtitle',
-            'With supporting text below as a natural lead-in to additional content.',
-            true,
-            true,
-            true,
-            'primary',
-            null
-        ),
-        t(
-            7,
-            'https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180',
-            null,
-            null,
-            null,
-            false,
-            false,
-            false,
-            null,
-            null
-        )
-    ]
+    let connection;
+    mysql.createConnection(db_conf)
+        .then(function (conn) {
+            connection = conn;
+        })
+        .then(function(){
+            return connection.query(`SELECT * FROM ${db_conf.database}.news`);
+        })
+        .then((rows) => {
 
-    const filteredList = items
-        .filter( item => !filter ||
-            (item.cardTitle && item.cardTitle.indexOf(filter)>-1) ||
-            (item.cardSubtitle && item.cardSubtitle.indexOf(filter)>-1) ||
-            (item.cardText && item.cardText.indexOf(filter)>-1)
-        )
+            res.send(
+                {
+                    items: rows,
+                    count: rows.length
+                }
+            );
 
-    setTimeout( () => {
-        res.send(
-            {
-                items: filteredList,
-                count: filteredList.length
-            }
-        );
-    }, 1000 )
+            connection.end();
+        })
+        .catch((error) => {
+            res.sendStatus(500);
+            if (connection && connection.end) connection.end();
+            console.log(error);
+        })
 
 }
 
